0cc3aea042e012cfa1001b0050a7060e6fce7ea9
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-11-21 22:05:27 -0500

    add a request to validate the fields

---
+++ app/Http/Requests/StoreTask.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTask extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255|min:5',
+            'description' => 'required|string',
+            'long_description' => 'required|string',
+            // 'completed' => 'required|boolean',
+        ];
+    }
+}
