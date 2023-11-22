a61396c01b5672fb73a43d6c29c22d8fd58650b1
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-11-21 22:05:49 -0500

    update the controller (create - store)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTask;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,18 @@ class TaskController extends Controller
     {
         $task = Task::findOrFail($id);
         return view('task.show', ['task' => $task]);
+    }
+
+    public function create()
+    {
+        //return "Create a task";
+        //dd('Create a task');
+        return view('task.create');
+    }
+
+    public function store(StoreTask $request)
+    {
+        $task = Task::create($request->all());
+        return redirect()->route('tasks.show', $task->id)->with('status', 'Task created successfully!');
     }
 }
