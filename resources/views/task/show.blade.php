8ea699a0394bf5e6e2cb54a879827e3683273d4d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-11-22 22:15:57 -0500

    add a bottom

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -10,6 +10,21 @@
     <p>Created at: {{ $task->created_at }}</p>
     <p>Updated at: {{ $task->updated_at }}</p>
 
+    <p>Task {{ $task->completed ? 'Completed' : 'Not Completed' }}</p>
+
     <p><a href="{{ route('tasks.edit', $task->id) }}">Edit Task</a></p>
     <p><a href="{{ route('tasks.index') }}">Back to all tasks</a></p>
+    <div>
+        <form action="{{ route('tasks.toggle-completed', $task) }}" method="POST">
+            @csrf
+            @method('PATCH')
+            <button type="submit">{{ $task->completed ? 'not Completed' : 'Completed' }}</button>
+        </form>
+
+        <form action="{{ route('tasks.destroy', $task) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete Task</button>
+        </form>
+    </div>
 @endsection
