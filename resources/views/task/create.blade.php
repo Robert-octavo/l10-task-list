e41ae0c6d4794fbfb697b5e59cde498377b8ec2d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-11-23 21:39:09 -0500

    add style - use alpine

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -1,42 +1,38 @@
 @extends('layouts.app')
 @section('title', 'Create a New Task')
 @section('styles')
-    <style>
-        .error {
-            color: red;
-            font-size: .8rem;
-        }
-    </style>
+
 @endsection
 @section('content')
     <form action="{{ route('tasks.store') }}" method="POST">
         @csrf
-        <div>
+        <div class="mb-4">
             <label for="title">Title:</label>
             <br>
-            <input type="text" name="title" id="title" value="{{ old('title') }}">
+            <input type="text" name="title" id="title" value="{{ old('title') }}" @class(['border-red-500', $errors->has('title')])>
             @error('title')
                 <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="description">Description:</label>
             <br>
-            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
+            <textarea name="description" id="description" @class(['border-red-500', $errors->has('description')]) rows="5">{{ old('description') }}</textarea>
             @error('description')
                 <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
+        <div class="mb-4">
             <label for="long_description">Long Description:</label>
             <br>
-            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
+            <textarea name="long_description" id="long_description" @class(['border-red-500', $errors->has('long_description')]) rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
                 <p class="error">{{ $message }}</p>
             @enderror
         </div>
-        <div>
-            <button type="submit">Create Task</button>
+        <div class="flex gap-2">
+            <button type="submit" class="btn mt-4 bg-gray-500/50">Create Task</button>
+            <a href="{{ route('tasks.index') }}" class="btn mt-4 bg-gray-500/50">Go back to the tasks list</a>
         </div>
     </form>
 
